c5dc83811759dbfb60c54bac26b2060fd534a13c
CrazyFay <2522235302@qq.com>
2022-10-17 14:47:59 +0800

    update bitmap and bloomfilter

---
+++ bloomfilter/bitmap.go
@@ -0,0 +1,47 @@
+package bloomfilter
+
+type Filter []byte
+
+func MakeBitmapWithByteSize(nBytes int) Filter {
+	filter := make([]byte, nBytes)
+	return filter
+}
+
+func MakeBitmapWithBitSize(nBits int) Filter {
+	if nBits < 64 {
+		nBits = 64
+	}
+	return MakeBitmapWithByteSize((nBits + 7) / 8)
+}
+
+func (f Filter) SetHashNum(hashNum uint8) {
+	f[len(f)-1] = byte(hashNum)
+}
+
+// Insert Change the bitPos th bit to 1
+func (f Filter) Insert(bitPos uint32) {
+	f[bitPos/8] |= 1 << (bitPos % 8)
+}
+
+// Delete Change the bitPos th bit to 0
+func (f Filter) Delete(bitPos uint32) {
+	f[bitPos/8] &= ^(1 << (bitPos % 8))
+}
+
+func (f Filter) Contains(bitPos uint32) bool {
+	return f[bitPos/8]&(1<<(bitPos%8)) != 0
+}
+
+func (f Filter) Reset() {
+	for i := range f {
+		f[i] = 0
+	}
+}
+
+func (f Filter) ByteSize(bitPos uint32) int {
+	return len(f)
+}
+
+func (f Filter) BitSize(bitPos uint32) int {
+	return len(f) * 8
+}
--- bloomfilter/bloomfilter.go
+++ bloomfilter/bloomfilter.go
@@ -1,97 +1,133 @@
 package bloomfilter
 
-import (
-	"math"
-)
+import "math"
 
-type Filter struct {
-	// todo make it be a bitmap
-	filter  []bool
-	hashNum int
+type BloomFilter struct {
+	bitmap Filter
+	k      uint8 // hash Function nums
 }
 
-// NewFilter returns a new Bloom filter that encodes a set of []byte keys with
-// the given number of bits per key, approximately.
-//
-// A good bitsPerKey value is 10, which yields a filter with ~ 1% false
-// positive rate.
-func NewFilter(keys []uint32, numEntries int, fp float64) *Filter {
-	f := &Filter{}
-	f.hashNum = f.bitsPerKey(numEntries, fp)
-	f.filter = f.appendFilter(keys, f.hashNum)
-	return f
+func (bf *BloomFilter) MayContainKey(k []byte) bool {
+	return bf.mayContain(Hash(k))
 }
 
-func NewFilterDefault(keys []uint32) *Filter {
-	f := &Filter{}
-	f.hashNum = 10
-	f.filter = f.appendFilter(keys, f.hashNum)
-	return f
+func (bf *BloomFilter) mayContain(h uint32) bool {
+	if bf.len() < 2 {
+		return false
+	}
+	k := bf.k
+	if k > 30 {
+		return true
+	}
+
+	nBits := uint32(8 * (bf.len() - 1))
+	delta := h>>17 | h<<15
+	for j := uint8(0); j < k; j++ {
+		bitPos := h & nBits
+		if !bf.bitmap.Contains(bitPos) {
+			return false
+		}
+		h += delta
+	}
+	return true
 }
 
-// BitsPerKey 指定假阳性率和数据量的情况下计算数组长度大小
-func (f *Filter) bitsPerKey(numEntries int, fp float64) int {
-	size := -1 * float64(numEntries) * math.Log(fp) / math.Pow(math.Ln2, 2)
+func (bf BloomFilter) len() int32 {
+	return int32(len(bf.bitmap))
+}
+
+func (bf *BloomFilter) InsertKey(k []byte) bool {
+	return bf.insert(Hash(k))
+}
+
+func (bf *BloomFilter) insert(h uint32) bool {
+	k := bf.k
+	if k > 30 {
+		return true
+	}
+	nBits := uint32(8 * (bf.len() - 1))
+	delta := h>>17 | h<<15
+	for j := uint8(0); j < k; j++ {
+		bitPos := h % uint32(nBits)
+		bf.bitmap.Insert(bitPos)
+		h += delta
+	}
+	return true
+}
+
+func (bf *BloomFilter) AllowKey(k []byte) bool {
+	if bf == nil {
+		return true
+	}
+	already := bf.MayContainKey(k)
+	if !already {
+		bf.InsertKey(k)
+	}
+	return already
+}
+
+func (bf *BloomFilter) Allow(h uint32) bool {
+	if bf == nil {
+		return true
+	}
+	already := bf.mayContain(h)
+	if !already {
+		bf.insert(h)
+	}
+	return already
+}
+
+func (bf *BloomFilter) Reset() {
+	if bf == nil {
+		return
+	}
+	for i := range bf.bitmap {
+		bf.bitmap[i] = 0
+	}
+}
+
+func NewBloomFilter(numEntries int, falsePositive float64) *BloomFilter {
+	bitsPerKey := bitsPerKey(numEntries, falsePositive)
+	return initFilter(numEntries, bitsPerKey)
+}
+
+func NewBloomFilterDefault(numEntries int) *BloomFilter {
+	return initFilter(numEntries, 10)
+}
+
+func bitsPerKey(numEntries int, falsePositive float64) int {
+	size := -1 * float64(numEntries) * math.Log(falsePositive) / math.Pow(math.Ln2, 2)
 	locs := math.Ceil(size / float64(numEntries))
 	return int(locs)
 }
 
-// CalcHashNum 通过指定的数组长度计算最合适的hash数量
-func (f *Filter) calcHashNum(bitsPerKey int) (k uint32) {
-	k = uint32(float64(bitsPerKey) * math.Ln2)
+func initFilter(numEntries int, bitsPerKey int) *BloomFilter {
+	bf := &BloomFilter{}
+	if bitsPerKey < 0 {
+		bitsPerKey = 0
+	}
+	k := uint32(float64(bitsPerKey) * math.Ln2)
 	if k < 1 {
 		k = 1
 	}
 	if k > 30 {
 		k = 30
 	}
-	return
-}
+	bf.k = uint8(k)
 
-// appendFilter 将多个key放入bloomFilter中
-func (f *Filter) appendFilter(keys []uint32, bitsPerKey int) []bool {
-	if bitsPerKey < 0 {
-		bitsPerKey = 0
+	nBits := numEntries * int(bitsPerKey)
+	if nBits < 64 {
+		nBits = 64
 	}
-	k := f.calcHashNum(bitsPerKey)
+	// bitmap`s []byte length
+	nBytes := (nBits + 7) / 8
+	nBits = nBytes * 8
+	filter := MakeBitmapWithByteSize(nBytes + 1)
 
-	nBits := len(keys) * bitsPerKey
+	filter.SetHashNum(uint8(k))
 
-	filter := make([]bool, nBits)
-
-	// 同一个hash函数做了k次的重复计算
-	for _, h := range keys {
-		delta := h>>17 | h<<15
-		for j := uint32(0); j < k; j++ {
-			bitPos := h % uint32(nBits)
-			filter[bitPos] = true
-			h += delta
-		}
-	}
-	return filter
-}
-
-// MayContainsKey 通过过滤器进行过滤
-func (f *Filter) MayContainsKey(key []byte) bool {
-	return f.mayContain(Hash(key))
-}
-
-func (f *Filter) mayContain(key uint32) bool {
-	if len(f.filter) < 2 {
-		return false
-	}
-	k := f.calcHashNum(f.hashNum)
-
-	nBits := uint32(len(f.filter) - 1)
-	delta := key>>17 | key<<15
-	for j := uint32(0); j < k; j++ {
-		bitPos := key % nBits
-		if f.filter[bitPos] {
-			return false
-		}
-		key += delta
-	}
-	return true
+	bf.bitmap = filter
+	return bf
 }
 
 // Hash 采用类murmur Hash进行高效的哈希计算
