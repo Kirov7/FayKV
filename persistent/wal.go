7b7ede9145fb1598c07e10e36b3cc39f683c08dc
CrazyFay <2522235302@qq.com>
2022-11-03 14:33:57 +0800

    update wal

--- persistent/wal.go
+++ persistent/wal.go
@@ -2,6 +2,12 @@ package persistent
 
 import (
 	"bytes"
+	"encoding/binary"
+	"github.com/Kirov7/FayKV/inmemory"
+	"github.com/Kirov7/FayKV/utils"
+	"hash/crc32"
+	"io"
+	"log"
 	"os"
 	"sync"
 )
@@ -33,4 +39,71 @@ func (wf *WalFile) Name() string {
 
 func (wf *WalFile) Size() uint32 {
 	return wf.writeAt
+}
+
+func OpenWalFile(opt *Options) *WalFile {
+	fd, err := OpenMmapFile(opt.FileName, os.O_CREATE|os.O_RDWR, opt.MaxSize)
+	if err != nil {
+		log.Println(err)
+	}
+	wf := &WalFile{
+		lock: &sync.RWMutex{},
+		f:    fd,
+		opts: opt,
+		buf:  &bytes.Buffer{},
+		size: uint32(len(fd.Data)),
+	}
+	return wf
+}
+
+func (wf WalFile) Write(entry *inmemory.Entry) error {
+	panic("todo")
+}
+
+const maxHeaderSize int = 21
+
+type WalHeader struct {
+	KeyLen    uint32
+	ValueLen  uint32
+	Meta      byte
+	ExpiresAt uint64
+}
+
+func (h *WalHeader) Encode(out []byte) int {
+	index := 0
+	index = binary.PutUvarint(out[index:], uint64(h.KeyLen))
+	index += binary.PutUvarint(out[index:], uint64(h.ValueLen))
+	index += binary.PutUvarint(out[index:], uint64(h.Meta))
+	index += binary.PutUvarint(out[index:], h.ExpiresAt)
+	return index
+}
+
+func WalCodec(buf *bytes.Buffer, e *inmemory.Entry) int {
+	buf.Reset()
+	h := WalHeader{
+		KeyLen:    uint32(len(e.Key)),
+		ValueLen:  uint32(len(e.Value)),
+		ExpiresAt: e.ExpiresAt,
+	}
+
+	hash := crc32.New(utils.CastagnoliCrcTable)
+	writer := io.MultiWriter(buf, hash)
+
+	// encode header.
+	var headerEnc [maxHeaderSize]byte
+	sz := h.Encode(headerEnc[:])
+	utils.Panic2(writer.Write(headerEnc[:sz]))
+	utils.Panic2(writer.Write(e.Key))
+	utils.Panic2(writer.Write(e.Value))
+	// write crc32 hash.
+	var crcBuf [crc32.Size]byte
+	binary.BigEndian.PutUint32(crcBuf[:], hash.Sum32())
+	utils.Panic2(buf.Write(crcBuf[:]))
+	// return encoded length.
+	return len(headerEnc[:sz]) + len(e.Key) + len(e.Value) + len(crcBuf)
+}
+
+func EstimateWalCodecSize(e *inmemory.Entry) int {
+	return len(e.Key) + len(e.Value) + 8 /* ExpiresAt uint64 */ +
+		crc32.Size + maxHeaderSize
 }
--- utils/error.go
+++ utils/error.go
@@ -4,6 +4,7 @@ import "github.com/pkg/errors"
 
 var (
 	ErrChecksumMismatch = errors.New("checksum mismatch")
+	ErrEmptyKey         = errors.New("Key cannot be empty")
 )
 
 // Panic 如果err 不为nil 则panicc
@@ -11,4 +12,8 @@ func Panic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Panic2(_ interface{}, err error) {
+	Panic(err)
 }
