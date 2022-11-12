760a56b0e8f89e2df81433ade9ff45358e65836c
CrazyFay <2522235302@qq.com>
2022-11-12 21:42:34 +0800

    improve bloomfilter

--- cache/bloomfilter.go
+++ cache/bloomfilter.go
@@ -95,6 +95,11 @@ func NewBloomFilterDefault(numEntries int) *BloomFilter {
 	return initFilter(numEntries, 10)
 }
 
+func BuildBloomFilter(srcBytes []uint32, falsePositive float64) Filter {
+	bitsPerKey := bitsPerKey(len(srcBytes), falsePositive)
+	return buildFilter(srcBytes, bitsPerKey)
+}
+
 func bitsPerKey(numEntries int, falsePositive float64) int {
 	size := -1 * float64(numEntries) * math.Log(falsePositive) / math.Pow(math.Ln2, 2)
 	locs := math.Ceil(size / float64(numEntries))
@@ -128,6 +133,44 @@ func initFilter(numEntries int, bitsPerKey int) *BloomFilter {
 
 	bf.bitmap = filter
 	return bf
+}
+
+func buildFilter(bytes []uint32, bitsPerKey int) []byte {
+	if bitsPerKey < 0 {
+		bitsPerKey = 0
+	}
+	// 0.69 is approximately ln(2).
+	k := uint32(float64(bitsPerKey) * 0.69)
+	if k < 1 {
+		k = 1
+	}
+	if k > 30 {
+		k = 30
+	}
+
+	nBits := len(bytes) * int(bitsPerKey)
+	// For small len(keys), we can see a very high false positive rate. Fix it
+	// by enforcing a minimum bloom filter length.
+	if nBits < 64 {
+		nBits = 64
+	}
+	nBytes := (nBits + 7) / 8
+	nBits = nBytes * 8
+	filter := make([]byte, nBytes+1)
+
+	for _, h := range bytes {
+		delta := h>>17 | h<<15
+		for j := uint32(0); j < k; j++ {
+			bitPos := h % uint32(nBits)
+			filter[bitPos/8] |= 1 << (bitPos % 8)
+			h += delta
+		}
+	}
+
+	//record the K value of this Bloom Filter
+	filter[nBytes] = uint8(k)
+
+	return filter
 }
 
 // Hash 采用类murmur Hash进行高效的哈希计算
