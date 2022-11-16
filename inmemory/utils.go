1c01934c607e75c7abe388bbc50081597ba9b9c6
CrazyFay <2522235302@qq.com>
2022-11-16 22:05:12 +0800

    add comment

--- inmemory/utils.go
+++ inmemory/utils.go
@@ -9,6 +9,10 @@ import (
 	"math"
 )
 
+// CompareKeys checks the key without timestamp and checks the timestamp if keyNoTs
+// is same.
+// a<timestamp> would be sorted higher than aa<timestamp> if we use bytes.compare
+// All keys should have timestamp.
 func CompareKeys(key1, key2 []byte) int {
 	if len(key1) <= 8 || len(key2) <= 8 {
 		panic(fmt.Errorf("%s,%s < 8", string(key1), string(key2)))
