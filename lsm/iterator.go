e7220329b7487af3387c27309581528e0df0fdc5
CrazyFay <2522235302@qq.com>
2022-11-16 22:03:53 +0800

    init lsm Iterator

--- lsm/iterator.go
+++ lsm/iterator.go
@@ -1,1 +1,37 @@
 package lsm
+
+import "github.com/Kirov7/FayKV/utils"
+
+type Iterator struct {
+	it    Item
+	iters []utils.Iterator
+}
+type Item struct {
+	e *utils.Entry
+}
+
+func (it *Item) Entry() *utils.Entry {
+	return it.e
+}
+
+func (lsm *LSM) NewIterators(opt *utils.Options) []utils.Iterator {
+	panic("todo")
+}
+func (iter *Iterator) Next() {
+	iter.iters[0].Next()
+}
+func (iter *Iterator) Valid() bool {
+	return iter.iters[0].Valid()
+}
+func (iter *Iterator) Rewind() {
+	iter.iters[0].Rewind()
+}
+func (iter *Iterator) Item() utils.Item {
+	return iter.iters[0].Item()
+}
+func (iter *Iterator) Close() error {
+	return nil
+}
+
+func (iter *Iterator) Seek(key []byte) {
+}
