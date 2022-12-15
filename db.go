ffeaaf561f5c9fe5885bdead9c3818c24936d571
CrazyFay <2522235302@qq.com>
2022-12-15 11:47:43 +0800

    feat: init Open method in db.go

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Kirov7/FayKV/inmemory"
 	"github.com/Kirov7/FayKV/lsm"
 	"github.com/Kirov7/FayKV/utils"
+	"sync"
 )
 
 type KvAPI interface {
@@ -16,10 +17,39 @@ type KvAPI interface {
 }
 
 type DB struct {
+	sync.RWMutex
+	opt   *Options
 	stats *Stats
 	lsm   *lsm.LSM
 }
 
+func Open(opt *Options) *DB {
+	c := utils.NewCloser()
+	db := &DB{opt: opt}
+	// init LSM structure
+	db.lsm = lsm.NewLSM(&lsm.Options{
+		WorkDir:             opt.WorkDir,
+		MemTableSize:        opt.MemTableSize,
+		SSTableMaxSize:      opt.SSTableMaxSz,
+		BlockSize:           8 * 1024,
+		BloomFalsePositive:  0, //0.01,
+		BaseLevelSize:       10 << 20,
+		LevelSizeMultiplier: 10,
+		BaseTableSize:       5 << 20,
+		TableSizeMultiplier: 2,
+		NumLevelZeroTables:  15,
+		MaxLevelNum:         7,
+		NumCompactors:       1,
+	})
+	// Example Initialize statistics
+	db.stats = newStats(opt)
+	// Start the merge compression process for the sstable
+	go db.lsm.StartCompacter()
+	c.Add(1)
+	// todo init worker channel
+
+	return db
+}
 func (db *DB) Set(data *inmemory.Element) error {
 	// todo implement there
 	panic("todo")
