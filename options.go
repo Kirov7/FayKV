f147eaca77fcf8ae26655d66dab07445bcc031c2
CrazyFay <2522235302@qq.com>
2022-12-15 11:47:30 +0800

    feat: improve options properties and method

--- options.go
+++ options.go
@@ -1,5 +1,35 @@
 package FayKV
 
+import "github.com/Kirov7/FayKV/utils"
+
+type Options struct {
+	ValueThreshold      int64
+	WorkDir             string
+	MemTableSize        int64
+	SSTableMaxSz        int64
+	MaxBatchCount       int64
+	MaxBatchSize        int64 // max batch size in bytes
+	ValueLogFileSize    int
+	VerifyValueChecksum bool
+	ValueLogMaxEntries  uint32
+	LogRotatesToFlush   int32
+	MaxTableSize        int64
+}
+
 type Stats struct {
-	EntryNum int64 // 已存储entry数量
+	closer   *utils.Closer
+	EntryNum int64 // Number of stored entries
+}
+
+// NewStats
+func newStats(opt *Options) *Stats {
+	s := &Stats{}
+	s.closer = utils.NewCloser()
+	s.EntryNum = 1
+	return s
+}
+
+// Close
+func (s *Stats) close() error {
+	return nil
 }
